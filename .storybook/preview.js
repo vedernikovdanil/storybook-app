1b716029e4239eb12fe9d3ffd876aaddfd2ada28
vedernikovdanil <vedernikov.1974@hotmail.com>
2023-03-22 02:34:26 +0500

    Add global decorator, fix styles, remove webpack alias

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import '../src/scss/index.scss';
+import cn from 'classnames';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -9,3 +10,33 @@ export const parameters = {
     },
   },
 };
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: null,
+    toolbar: {
+      icon: 'circlehollow',
+      items: [
+        { value: null, title: 'none' },
+        'light',
+        'dark',
+        'primary',
+        'secondary',
+        'success',
+        'info',
+        'danger',
+        'warning',
+      ],
+      name: true,
+      dynamicTitle: true,
+    },
+  },
+};
+
+export const decorators = [
+  (story, { globals: { theme } }) => {
+    return <div className={cn('p-3', theme && `bg-${theme}`)}>{story()}</div>;
+  },
+];
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,4 @@
-const path = require('path');
-
 module.exports = {
-  resolve: {
-    alias: {
-      '@scss': path.resolve(__dirname, 'src/scss'),
-    },
-  },
   module: {
     rules: [
       {
