52314fbb6bbf4cee6fe29098cb13c153e9831d70
vedernikovdanil <vedernikov.1974@hotmail.com>
2023-04-16 05:07:04 +0500

    Fix Nav.Logo component

--- src/components/organisms/Navbar/Logo/index.tsx
+++ src/components/organisms/Navbar/Logo/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
 import cn from 'classnames';
+import { type BaseProps } from '~/src/types';
 import Nav from '@molecules/Nav';
 import { type NavLinkProps } from '@molecules/Nav/Link';
-import { type BaseProps } from '~/src/types';
 
-function Logo<As extends React.ElementType = 'a'>({
-  as = 'a' as As,
-  className,
-  ...props
-}: BaseProps<As, NavLinkProps>) {
-  return <Nav.Link className={cn('navbar-logo', className)} {...props} />;
+function Logo<As extends React.ElementType = 'a'>(
+  props: BaseProps<As, NavLinkProps>
+) {
+  return Nav.Link<As>({
+    className: cn('navbar-logo', props?.className),
+    ...props,
+  });
 }
 
 export default Logo;
